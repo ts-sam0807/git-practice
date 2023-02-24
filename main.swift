0aceea2fdbe2ce55672822c69ab937181c3f1885
feraltech <info@feraltech.hk>
2023-02-25 01:23:28 +0800

    Change a variable and values

--- main.swift
+++ main.swift
@@ -1,9 +1,9 @@
 print("Hello, Swift!")
-print("Hello, Swift!")
+print("Hello, World!!")
 print("Hello, Swift!")
 
 var a = 10
-var b = 10
-var c = 10
+var b = 20
+var c = 30
 print("a + b + c = \(a + b + c)")
 
